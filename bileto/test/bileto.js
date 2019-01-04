d04a334d94172a6b5e19e00ec92cc5c3ca0b1859
Fabio Correa <feamcor@gmail.com>
2019-01-04 15:48:12 -0300

    More test cases

--- bileto/test/bileto.js
+++ bileto/test/bileto.js
@@ -10,20 +10,21 @@ contract('Bileto', async (accounts) => {
   let __refundable;
   let __lastEvent;
   let __lastPurchase;
-
+  let __timestamp = (new Date).getTime();
+  
   before(async () => {
     __contract = await Bileto.deployed();
     __address = __contract.address;
     __owner = await __contract.owner();
-    let _counter = 0;
-    for (let _account of accounts) {
-      const _balance = await web3.eth.getBalance(_account);
-      console.log("account[" + _counter + "]:\t" + _account + " = " + web3.utils.fromWei(_balance, 'ether'));
-      _counter += 1;
-      if (_counter == 5) break;
-    }
-    console.log("store address:\t" + __address);
-    console.log("store owner:\t" + __owner);
+    // let _counter = 0;
+    // for (let _account of accounts) {
+      // const _balance = await web3.eth.getBalance(_account);
+      // console.log("account[" + _counter + "]:\t" + _account + " = " + web3.utils.fromWei(_balance, 'ether'));
+      // _counter += 1;
+      // if (_counter == 5) break;
+    // }
+    // console.log("store address:\t" + __address);
+    // console.log("store owner:\t" + __owner);
   });
 
   beforeEach(async () => {
@@ -32,11 +33,11 @@ contract('Bileto', async (accounts) => {
     __refundable = await __contract.storeRefundableBalance();
     __lastEvent = await __contract.eventsCounter();
     __lastPurchase = await __contract.purchasesCounter();
-    console.log("store status:\t" + __status);
-    console.log("store balance:\t" + __balance);
-    console.log("store refund:\t" + __refundable);
-    console.log("last event:\t" + __lastEvent);
-    console.log("last purchase:\t" + __lastPurchase);
+    // console.log("store status:\t" + __status);
+    // console.log("store balance:\t" + web3.utils.fromWei(__balance, "ether"));
+    // console.log("store refund:\t" + web3.utils.fromWei(__refundable, "ether"));
+    // console.log("last event:\t" + __lastEvent);
+    // console.log("last purchase:\t" + __lastPurchase);
   });
 
   it("should create store", async () => {
@@ -83,7 +84,8 @@ contract('Bileto', async (accounts) => {
   });
 
   it("should not create an event for non-owner", async () => {
-    await truffleAssert.reverts(__contract.createEvent(
+    await truffleAssert.reverts(
+      __contract.createEvent(
         "BILETO-EVENT-1",
         accounts[1],
         "BILETO EVENT 1",
@@ -92,7 +94,67 @@ contract('Bileto', async (accounts) => {
         10, {
           from: accounts[1]
         }),
-      "by owner");
+      "owner");
+  });
+
+  it("should not create an event when organizer is a contract", async () => {
+    await truffleAssert.reverts(
+      __contract.createEvent(
+        "BILETO-EVENT-1",
+        __address,
+        "BILETO EVENT 1",
+        1000,
+        web3.utils.toWei("0.1", "ether"),
+        10),
+      "contract");
+  });
+
+  it("should not create an event without external ID", async () => {
+    await truffleAssert.reverts(
+      __contract.createEvent(
+        "",
+        accounts[1],
+        "BILETO EVENT 1",
+        1000,
+        web3.utils.toWei("0.1", "ether"),
+        10),
+      "external ID");
+  });
+
+  it("should not create an event without name", async () => {
+    await truffleAssert.reverts(
+      __contract.createEvent(
+        "BILETO-EVENT-1",
+        accounts[1],
+        "",
+        1000,
+        web3.utils.toWei("0.1", "ether"),
+        10),
+      "name");
+  });
+
+  it("should not create an event with incentive greater than 100%", async () => {
+    await truffleAssert.reverts(
+      __contract.createEvent(
+        "BILETO-EVENT-1",
+        accounts[1],
+        "BILETO EVENT 1",
+        10001,
+        web3.utils.toWei("0.1", "ether"),
+        10),
+      "incentive");
+  });
+
+  it("should not create an event with no tickets available for sale", async () => {
+    await truffleAssert.reverts(
+      __contract.createEvent(
+        "BILETO-EVENT-1",
+        accounts[1],
+        "BILETO EVENT 1",
+        1000,
+        web3.utils.toWei("0.1", "ether"),
+        0),
+      "available");
   });
 
   it("should create an event", async () => {
@@ -103,7 +165,8 @@ contract('Bileto', async (accounts) => {
       1000,
       web3.utils.toWei("0.1", "ether"),
       10);
-    const _info = await __contract.fetchEventInfo.call(__lastEvent);
+    const _eventId = await __contract.eventsCounter();
+    const _info = await __contract.fetchEventInfo.call(_eventId);
     assert.strictEqual(_info._eventStatus.toNumber(), 0, "event status is not EventStatus.Created (0)");
     truffleAssert.eventEmitted(_result, "EventCreated");
   });
@@ -132,29 +195,179 @@ contract('Bileto', async (accounts) => {
     assert.strictEqual(_info._refundableBalance.toNumber(), 0, "event refundable balance should be zero");
   });
 
+  it("should not complete purchase sales not started yet", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        1,
+        "BILETO-EVENT-1-PURCHASE-1",
+        (new Date).getTime(),
+        "BILETO-CUSTOMER-1", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.1", "ether")
+        }),
+      "started");
+  });
+
   it("should start ticket sales of an event", async () => {
-    const _result = await __contract.startTicketSales(__lastEvent, {from: accounts[1]});
+    const _result = await __contract.startTicketSales(__lastEvent, {
+      from: accounts[1]
+    });
     const _info = await __contract.fetchEventInfo.call(__lastEvent);
     assert.strictEqual(_info._eventStatus.toNumber(), 1, "event status is not EventStatus.SalesStarted (1)");
     truffleAssert.eventEmitted(_result, "EventSalesStarted");
   });
 
+  it("should not complete purchase when quantity is zero", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        0,
+        "BILETO-EVENT-1-PURCHASE-1",
+        (new Date).getTime(),
+        "BILETO-CUSTOMER-1", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.1", "ether")
+        }),
+      "quantity");
+  });
+
+  it("should not complete purchase when there are not enough tickets", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        100,
+        "BILETO-EVENT-1-PURCHASE-1",
+        (new Date).getTime(),
+        "BILETO-CUSTOMER-1", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.1", "ether")
+        }),
+      "not enough");
+  });
+
+  it("should not complete purchase without external ID", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        1,
+        "",
+        (new Date).getTime(),
+        "BILETO-CUSTOMER-1", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.1", "ether")
+        }),
+      "external ID");
+  });
+
+  it("should not complete purchase without timestamp", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        1,
+        "BILETO-EVENT-1-PURCHASE-1",
+        0,
+        "BILETO-CUSTOMER-1", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.1", "ether")
+        }),
+      "purchase date");
+  });
+
+  it("should not complete purchase without customer ID", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        1,
+        "BILETO-EVENT-1-PURCHASE-1",
+        (new Date).getTime(),
+        "", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.1", "ether")
+        }),
+      "customer ID");
+  });
+
+  it("should not complete purchase value less than total", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        1,
+        "BILETO-EVENT-1-PURCHASE-1",
+        (new Date).getTime(),
+        "BILETO-CUSTOMER-1", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.01", "ether")
+        }),
+      "customer funds");
+  });
+
+  it("should not complete purchase value more than total", async () => {
+    await truffleAssert.reverts(
+      __contract.purchaseTickets(
+        __lastEvent,
+        1,
+        "BILETO-EVENT-1-PURCHASE-1",
+        (new Date).getTime(),
+        "BILETO-CUSTOMER-1", {
+          from: accounts[2],
+          value: web3.utils.toWei("0.11", "ether")
+        }),
+      "customer funds");
+  });
+
+  it("should complete a purchase", async () => {
+    const _result = await __contract.purchaseTickets(
+      __lastEvent,
+      1,
+      "BILETO-EVENT-1-PURCHASE-1",
+      __timestamp,
+      "BILETO-CUSTOMER-1", {
+        from: accounts[2],
+        value: web3.utils.toWei("0.1", "ether")
+      });
+    const _purchaseId = await __contract.purchasesCounter();
+    const _info = await __contract.fetchPurchaseInfo.call(_purchaseId);
+    assert.strictEqual(_info._purchaseStatus.toNumber(), 0, "purchase status is not PurchaseStatus.Completed (0)");
+    truffleAssert.eventEmitted(_result, "PurchaseCompleted");
+  });
+
+  it("should store purchase info accordingly", async () => {
+    const _info = await __contract.fetchPurchaseInfo.call(__lastPurchase);
+    const _hash1 = web3.utils.keccak256("BILETO-EVENT-1-PURCHASE-1");
+    const _hash2 = web3.utils.keccak256("BILETO-CUSTOMER-1");
+    assert.strictEqual(_info._purchaseStatus.toString(), "0", "purchase status is not PurchaseStatus.Completed (0)");
+    assert.strictEqual(_info._externalId, _hash1, "purchase external ID hash is incorrect");
+    assert.strictEqual(_info._timestamp.toString(), __timestamp.toString(), "purchase timestamp is incorrect")
+    assert.strictEqual(_info._customer, accounts[2], "customer address is incorrect");
+    assert.strictEqual(_info._customerId, _hash2, "customer external ID hash is incorrect");
+    assert.strictEqual(_info._quantity.toString(), "1", "quantity of tickets purchased is incorrect");
+    assert.strictEqual(_info._total.toString(), web3.utils.toWei("0.1", "ether"), "event ticket price is incorrect");
+    assert.strictEqual(_info._eventId.toString(), __lastEvent.toString(), "event ID is incorrect");
+  });
+
   it("should suspend ticket sales of an event", async () => {
-    const _result = await __contract.suspendTicketSales(__lastEvent, {from: accounts[1]});
+    const _result = await __contract.suspendTicketSales(__lastEvent, {
+      from: accounts[1]
+    });
     const _info = await __contract.fetchEventInfo.call(__lastEvent);
     assert.strictEqual(_info._eventStatus.toNumber(), 2, "event status is not EventStatus.SalesSuspended (2)");
     truffleAssert.eventEmitted(_result, "EventSalesSuspended");
   });
 
   it("should end ticket sales of an event", async () => {
-    const _result = await __contract.endTicketSales(__lastEvent, {from: accounts[1]});
+    const _result = await __contract.endTicketSales(__lastEvent, {
+      from: accounts[1]
+    });
     const _info = await __contract.fetchEventInfo.call(__lastEvent);
     assert.strictEqual(_info._eventStatus.toNumber(), 3, "event status is not EventStatus.SalesFinished (3)");
     truffleAssert.eventEmitted(_result, "EventSalesFinished");
   });
 
   it("should complete an event", async () => {
-    const _result = await __contract.completeEvent(__lastEvent, {from: accounts[1]});
+    const _result = await __contract.completeEvent(__lastEvent, {
+      from: accounts[1]
+    });
     const _info = await __contract.fetchEventInfo.call(__lastEvent);
     assert.strictEqual(_info._eventStatus.toNumber(), 4, "event status is not EventStatus.Completed (4)");
     truffleAssert.eventEmitted(_result, "EventCompleted");
@@ -168,15 +381,14 @@ contract('Bileto', async (accounts) => {
   });
 
   it("should cancel an event", async () => {
-    const _result = await __contract.cancelEvent(__lastEvent, {from: accounts[1]});
+    const _result = await __contract.cancelEvent(__lastEvent, {
+      from: accounts[1]
+    });
     const _info = await __contract.fetchEventInfo.call(__lastEvent);
     assert.strictEqual(_info._eventStatus.toNumber(), 6, "event status is not EventStatus.Cancelled (6)");
     truffleAssert.eventEmitted(_result, "EventCancelled");
   });
 
-  it("should complete a purchase", async () => {
-
-  });
 
   it("should cancel a purchase", async () => {
 
@@ -195,7 +407,7 @@ contract('Bileto', async (accounts) => {
       __contract.closeStore({
         from: accounts[1]
       }),
-      "by owner");
+      "owner");
   });
 
   it("should close store", async () => {
