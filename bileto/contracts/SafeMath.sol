0a6b463d1ce5a62cf26b81db60e255051e669939
Fabio Correa <feamcor@gmail.com>
2019-01-05 14:24:42 -0300

    Upgrade to openzeppelin 2.1.1

--- bileto/contracts/SafeMath.sol
+++ bileto/contracts/SafeMath.sol
@@ -5,8 +5,6 @@ pragma solidity 0.5.2;
  * @dev Math operations with safety checks that revert on error
  */
 library SafeMath {
-    int256 constant private INT256_MIN = -2**255;
-
     /**
     * @dev Multiplies two unsigned integers, reverts on overflow.
     */
@@ -25,45 +23,13 @@ library SafeMath {
     }
 
     /**
-    * @dev Multiplies two signed integers, reverts on overflow.
-    */
-    function mul(int256 a, int256 b) internal pure returns (int256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (a == 0) {
-            return 0;
-        }
-
-        // This is the only case of overflow not detected by the check below
-        require(!(a == -1 && b == INT256_MIN), "multiplication has overflown");
-
-        int256 c = a * b;
-        require(c / a == b, "multiplication has overflown");
-
-        return c;
-    }
-
-    /**
     * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         // Solidity only automatically asserts when dividing by 0
-        require(b > 0, "cannot divide by zero");
+        require(b > 0);
         uint256 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two signed integers truncating the quotient, reverts on division by zero.
-    */
-    function div(int256 a, int256 b) internal pure returns (int256) {
-        require(b != 0, "cannot divide by zero"); // Solidity only automatically asserts when dividing by 0
-        require(!(b == -1 && a == INT256_MIN), "division has overflown"); // This is the only case of overflow
-
-        int256 c = a / b;
 
         return c;
     }
@@ -79,31 +45,11 @@ library SafeMath {
     }
 
     /**
-    * @dev Subtracts two signed integers, reverts on overflow.
-    */
-    function sub(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a - b;
-        require((b >= 0 && c <= a) || (b < 0 && c > a), "subtraction has overflown");
-
-        return c;
-    }
-
-    /**
     * @dev Adds two unsigned integers, reverts on overflow.
     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a, "addition has overflown");
-
-        return c;
-    }
-
-    /**
-    * @dev Adds two signed integers, reverts on overflow.
-    */
-    function add(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a + b;
-        require((b >= 0 && c >= a) || (b < 0 && c < a), "addition has overflown");
 
         return c;
     }
