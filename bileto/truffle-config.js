3bfac73ab1573409c667a15adef90928f8a2fb70
Fabio Correa <feamcor@gmail.com>
2019-01-09 22:57:21 -0300

    Enabled solc optimizer
    It was running out of gas during compilation.

--- bileto/truffle-config.js
+++ bileto/truffle-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -22,12 +24,11 @@
  *
  */
 
-require('dotenv').config();
-const HDWallet = require('truffle-hdwallet-provider');
-const infuraProjectId = process.env["INFURA_PROJECT_ID"];
-const metamaskSeedPhrase = process.env["METAMASK_SEED_PHRASE"];
-const rinkebyAddressIndex = process.env["RINKEBY_ADDRESS_INDEX"];
-const rinkebyNumAddresses = process.env["RINKEBY_NUM_ADDRESSES"];
+const HDWallet = require("truffle-hdwallet-provider");
+const infuraProjectId = process.env.INFURA_PROJECT_ID;
+const metamaskSeedPhrase = process.env.METAMASK_SEED_PHRASE;
+const rinkebyAddressIndex = process.env.RINKEBY_ADDRESS_INDEX;
+const rinkebyNumAddresses = process.env.RINKEBY_NUM_ADDRESSES;
 
 module.exports = {
   // plugins: [ "truffle-analyze" ],
@@ -49,48 +50,50 @@ module.exports = {
     // options below to some value.
     //
     development: {
-      host: "127.0.0.1",     // Localhost
-      port: 8545,            // Ganache CLI
+      host: "127.0.0.1", // Localhost
+      port: 8545, // Ganache CLI
       // port: 7545,            // Ganache GUI
-      network_id: "*",       // Any network (default: none)
+      network_id: "*" // Any network (default: none)
     },
 
     // Another network with more advanced options...
     // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+    // network_id: 3,       // Ropsten's id
+    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
 
     rinkeby: {
-      provider: () => new HDWallet(
-        metamaskSeedPhrase,
-        `https://rinkeby.infura.io/v3/${infuraProjectId}`,
-        rinkebyAddressIndex,
-        rinkebyNumAddresses),
+      provider: () =>
+        new HDWallet(
+          metamaskSeedPhrase,
+          `https://rinkeby.infura.io/v3/${infuraProjectId}`,
+          rinkebyAddressIndex,
+          rinkebyNumAddresses
+        ),
       network_id: 4,
       skipDryRun: true
-    },
+    }
 
     // Useful for private networks
     // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
 
@@ -102,16 +105,17 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.2",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.2", // Fetch exact version from solc-bin (default: truffle's version)
       // version: "native",
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true
+          //  runs: 200
+        }
+        //  evmVersion: "byzantium"
+      }
     }
   }
-}
+};
