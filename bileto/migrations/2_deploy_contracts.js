bd931a7b4dc803fb3313f101340d3d327861834c
Fabio Correa <feamcor@gmail.com>
2019-01-21 16:02:15 -0300

    Revert "Reverting to individual deployment of OpenZeppelin"
    This reverts commit b8f62f4abaa6e0da6b9acdddd46b34778cefa4ce.

--- bileto/migrations/2_deploy_contracts.js
+++ bileto/migrations/2_deploy_contracts.js
@@ -3,17 +3,17 @@ require("dotenv").config();
 // These OpenZeppelin libraries expose "internal" functions
 // which end up being linked inline with the caller contract.
 
-var Address = artifacts.require("./Address.sol");
-var Counter = artifacts.require("./Counter.sol");
-var SafeMath = artifacts.require("./SafeMath.sol");
+// var Address = artifacts.require("./Address.sol");
+// var Counter = artifacts.require("./Counter.sol");
+// var SafeMath = artifacts.require("./SafeMath.sol");
 var Bileto = artifacts.require("./Bileto.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Address);
-  deployer.deploy(Counter);
-  deployer.deploy(SafeMath);
-  deployer.link(Address, Bileto);
-  deployer.link(Counter, Bileto);
-  deployer.link(SafeMath, Bileto);
+  // deployer.deploy(Address);
+  // deployer.deploy(Counter);
+  // deployer.deploy(SafeMath);
+  // deployer.link(Address, Bileto);
+  // deployer.link(Counter, Bileto);
+  // deployer.link(SafeMath, Bileto);
   deployer.deploy(Bileto, process.env.STORE_NAME);
 };
