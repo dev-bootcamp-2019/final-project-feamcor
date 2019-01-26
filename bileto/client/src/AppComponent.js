0b549bd5502eef566eecfc52ca21c5674263de51
Fabio Correa <feamcor@gmail.com>
2019-01-26 15:40:38 -0300

    Current account and store info

--- bileto/client/src/AppComponent.js
+++ bileto/client/src/AppComponent.js
@@ -1,43 +1,120 @@
 import React, { Component } from "react";
+import {
+  AccountData,
+  ContractData,
+  ContractForm
+} from "drizzle-react-components";
 import PropTypes from "prop-types";
-
-import AppNavBar from "./AppNavBar";
-import StoreCard from "./StoreCard";
-import OpenStoreCard from "./OpenStoreCard";
-import SuspendStoreCard from "./SuspendStoreCard";
-import CloseStoreCard from "./CloseStoreCard";
-import AccountCard from "./AccountCard";
+import logo from "./tickets.png";
 
 class AppComponent extends Component {
   render() {
-    const { accounts, accountBalances, drizzleStatus } = this.props;
-    const { web3, contracts } = this.context.drizzle;
     return (
       <React.Fragment>
-        <AppNavBar contracts={contracts} />
+        <nav className="navbar sticky-top navbar-dark bg-dark">
+          <span className="navbar-brand">
+            <img
+              src={logo}
+              width="64"
+              height="64"
+              className="d-inline-block align-middle"
+              alt="bileto-logo"
+            />
+            <strong> Bileto </strong>a simple ticket store on Ethereum
+            <div className="navbar-text text-muted">
+              <small>
+                &nbsp;&nbsp;&nbsp;
+                {this.context.drizzle.contracts.Bileto.address}
+              </small>
+            </div>
+          </span>
+        </nav>
         <div className="container">
           <div className="row mt-3">
-            <div className="col-6">
-              <StoreCard />
+            <div className="col">
+              <div className="card shadow text-white bg-primary">
+                <h5 className="card-header">Account</h5>
+                <div className="card-body">
+                  <AccountData
+                    contract="Bileto"
+                    accountIndex="0"
+                    units="ether"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="col-6">
-              <AccountCard
-                accounts={accounts}
-                accountBalances={accountBalances}
-                drizzleStatus={drizzleStatus}
-                web3={web3}
-              />
+          </div>
+          <div className="row mt-3">
+            <div className="col-8">
+              <div className="card shadow h-100">
+                <h5 className="card-header">Store</h5>
+                <div className="card-body">
+                  <ContractData contract="Bileto" method="fetchStoreInfo" />
+                </div>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="row">
+                <div className="col">
+                  <div className="card shadow text-white bg-primary">
+                    <h5 className="card-header">Account Role</h5>
+                    <div className="card-body">
+                      <ContractData
+                        contract="Bileto"
+                        method="getAccountRole"
+                        methodArgs={[this.props.accounts[0]]}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col">
+                  <div className="card shadow border-info">
+                    <h5 className="card-header">Store Status</h5>
+                    <ul className="list-group list-group-flush">
+                      <li className="card-text list-group-item">0 - Created</li>
+                      <li className="list-group-item">1 - Open</li>
+                      <li className="list-group-item">2 - Suspended</li>
+                      <li className="list-group-item">3 - Closed</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="row mt-3">
             <div className="col-md-4">
-              <OpenStoreCard />
+              <div className="card shadow text-white bg-success h-100">
+                <h5 className="card-header">Open Store</h5>
+                <div className="card-body">
+                  <p className="card-text">
+                    Enable store to manage all events and ticket purchases.
+                  </p>
+                  <ContractForm contract="Bileto" method="openStore" />
+                </div>
+              </div>
             </div>
             <div className="col-md-4">
-              <SuspendStoreCard />
+              <div className="card shadow text-white bg-warning h-100">
+                <h5 className="card-header">Suspend Store</h5>
+                <div className="card-body">
+                  <p className="card-text">
+                    Temporarily disable store to manage all events and ticket
+                    purchases.
+                  </p>
+                  <ContractForm contract="Bileto" method="suspendStore" />
+                </div>
+              </div>
             </div>
             <div className="col-md-4">
-              <CloseStoreCard />
+              <div className="card shadow text-white bg-danger h-100">
+                <h5 className="card-header">Close Store</h5>
+                <div className="card-body">
+                  <p className="card-text">Disable store (cannot be reopen).</p>
+                  <ContractForm contract="Bileto" method="closeStore" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
